df265a56491d95124bd56823977ece6223871f14
Marius <marius.shiman@gmail.com>
2018-05-21 18:24:55 +0300

    Add rest CRUD operations

--- src/java/ua/phones/db/controllers/PhonesRest.java
+++ src/java/ua/phones/db/controllers/PhonesRest.java
@@ -3,9 +3,7 @@ package ua.phones.db.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ua.phones.db.models.Smartphone;
 import ua.phones.db.services.interfaces.PhonesTableService;
 
@@ -21,12 +19,33 @@ public class PhonesRest {
         this.phonesTableService = phonesTableService;
     }
 
-    @RequestMapping(value = "/phones", method = RequestMethod.GET)
+    @RequestMapping(value = "/phone", method = RequestMethod.GET)
     public ResponseEntity<List<Smartphone>> listAllSmartphones() {
         List<Smartphone> phones = phonesTableService.getAllSmartphones();
         if(phones.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<List<Smartphone>>(phones, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/phone/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deletePhone(@PathVariable(required = false) int id) {
+        if (id != 0) {
+            System.out.println("deleted " + id);
+            phonesTableService.deleteSmartPhone(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "/phone", method = RequestMethod.POST)
+    public ResponseEntity<Void> createSmartphon(@RequestBody Smartphone smartphone) {
+        if (smartphone != null) {
+            phonesTableService.addSmartPhone(smartphone);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
